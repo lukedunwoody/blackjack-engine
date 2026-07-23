1ed0790a5c936d0d90a4bb3986b7c7adaae08d40
ChopstixOfury <luked4422+github@gmail.com>
2026-07-22 23:44:36 -0400

    Add LUT table to track hit simulations for hands with two cards

--- scripts/cached_hit_sims_calc.c
+++ scripts/cached_hit_sims_calc.c
@@ -22,6 +22,17 @@ typedef struct {
     int size;
 } CacheTable;
 
+typedef struct {
+    int8_t card0;
+    int8_t card1;
+    int64_t sims;
+} LutEntry;
+
+typedef struct {
+    LutEntry list[55];
+    int size;
+} LutTable;
+
 int get_value(Hand hand) {
     int value = 0;
 
@@ -45,14 +56,14 @@ Hand sort_hand(Hand hand) {
     return hand;
 }
 
-int are_hands_equal(Hand hand1, Hand hand2) {
-    if (hand1.size != hand2.size) {
+int are_hands_equal(Hand hand0, Hand hand1) {
+    if (hand0.size != hand1.size) {
         return 0;
     }
 
     // At this point the hands are the same size
-    for (int i = 0; i < hand1.size; i++) {
-        if (hand1.cards[i] != hand2.cards[i]) {
+    for (int i = 0; i < hand0.size; i++) {
+        if (hand0.cards[i] != hand1.cards[i]) {
             return 0;
         }
     }
@@ -87,7 +98,17 @@ void add_cache(Hand hand, uint64_t sims, CacheTable *cache_table_ptr) {
     cache_table_ptr->size++;
 }
 
-uint64_t cached_hit(Hand hand, CacheTable *cache_table_ptr) {
+void add_lut(Hand hand, uint64_t sims, LutTable *lut_table_ptr) {
+    // Assume hand size is 2
+    LutEntry lut_entry;
+    lut_entry.card0 = hand.cards[0];
+    lut_entry.card1 = hand.cards[1];
+
+    lut_table_ptr->list[lut_table_ptr->size] = lut_entry;
+    lut_table_ptr->size++;
+}
+
+uint64_t cached_hit(Hand hand, CacheTable *cache_table_ptr, LutTable *lut_table_ptr) {
     uint64_t sims = 0;
 
     for (int i = 0; i < CARDS_LENGTH; i++) {
@@ -119,25 +140,39 @@ uint64_t cached_hit(Hand hand, CacheTable *cache_table_ptr) {
 
         // Check if 21 or card limit before calling again
         if (value != 21 && !(value == 11 && has_ace(new_hand)) && new_hand.size < HAND_SIZE_LIMIT) {
-            sims += cached_hit(new_hand, cache_table_ptr);
+            sims += cached_hit(new_hand, cache_table_ptr, lut_table_ptr);
         }
     }
     add_cache(hand, sims, cache_table_ptr);
+
+    if (hand.size == 2) {
+        add_lut(hand, sims, lut_table_ptr);
+    }
     return sims;
 }
 
 int main() {
     uint64_t sims = 0;
+
     CacheTable cache_table;
     cache_table.size = 0;
+
+    LutTable lut_table;
+    lut_table.size = 0;
 
     for (int i = 0; i < CARDS_LENGTH; i++) {
         Hand hand;
         hand.cards[0] = CARDS[i];
         hand.size = 1;
 
-        sims += cached_hit(hand, &cache_table);
+        sims += cached_hit(hand, &cache_table, &lut_table);
     }
+    FILE *file = fopen("../data/luts/two_card_hit_sims.bin", "wb");
+    if (file != NULL) {
+        fwrite(&lut_table, sizeof(LutTable), 1, file);
+        fclose(file);
+    }
+
     printf("Maximum simulations per hit: %llu\n", sims);
     getchar();
 }
